83ba19772f7df6ae99986a89504ff6532ae9e4a3
Drey037 <104416862+Drey037@users.noreply.github.com>
2024-01-31 00:10:30 +0800

    Single-Threaded merge sort works, multithreaded still buggy

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,118 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+import java.util.concurrent.*;
 
 public class Main {
     public static void main(String[] args) {
         // TODO: Seed your randomizer
+        Random rand = new Random(123);
 
         // TODO: Get array size and thread count from user
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter array size: ");
+        int array_size = sc.nextInt();
+        System.out.print("Enter thread count: ");
+        int thread_count = sc.nextInt();
+
+        if (thread_count == 0) {
+            System.out.println("Number of threads cannot be 0.");
+            System.exit(1);
+        }
 
         // TODO: Generate a random array of given size
+        Integer[] integers = new Integer[array_size];
+        for (int i = 0; i < array_size; i++) {
+            integers[i] = i+1;
+        }
 
-        // TODO: Call the generate_intervals method to generate the merge 
-        // sequence
+        // Shuffle list and turn it into int array
+        List<Integer> listArray = Arrays.asList(integers);
+        Collections.shuffle(listArray, rand);
+        int[] array = new int[array_size];
+        for (int i = 0; i < array_size; i++) {
+            array[i] = listArray.get(i);
+        }
+
+
+        //TEMP DISPLAY SHUFFLED ARRAY
+
+        System.out.println("SHUFFLED ARRAY:");
+        for (int i = 0; i < array_size; i++) {
+            System.out.print(array[i] + " ");
+        }
+        System.out.println("\n");
+
+
+
+        // TODO: Call the generate_intervals method to generate the merge sequence
+        List<Interval> intervals = generate_intervals(0, array_size-1);
 
         // TODO: Call merge on each interval in sequence
+        long startTime;
 
-        // Once you get the single-threaded version to work, it's time to 
+        // if thread count is 1
+        if (thread_count == 1) {
+            // Record the start time
+            startTime = System.currentTimeMillis();
+
+            for (Interval interval : intervals) {
+                merge(array, interval.getStart(), interval.getEnd());
+            }
+        }
+        else {
+            BlockingQueue<Interval> queue = new ArrayBlockingQueue<>(intervals.size());
+            queue.addAll(intervals);
+
+            ExecutorService executorService = Executors.newFixedThreadPool(thread_count);
+
+            // Record the start time
+            startTime = System.currentTimeMillis();
+
+            for (int i = 0; i < thread_count; i++) {
+                executorService.submit(new thread(array, queue));
+            }
+            executorService.shutdown();
+
+            // Wait until all tasks are finished
+            try {
+                executorService.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+
+        // Record the end time
+        long endTime = System.currentTimeMillis();
+
+        //TEMP DISPLAY SORTED ARRAY
+        System.out.println("\nSORTED ARRAY:");
+        for (int i = 0; i < array_size; i++) {
+            System.out.print(array[i] + " ");
+        }
+        System.out.println("\n");
+
+        System.out.println("\nRuntime: " + (endTime - startTime) + " milliseconds");
+
+
+        // SANITY CHECK
+        for (int i = 0; i < array_size; i++) {
+            if (i != array_size-1 && array[i] != i+1) {
+                System.out.println("Array is not sorted.");
+                System.out.println("array[" + i + "] = " + array[i]);
+            }
+        }
+
+
+
+        // Once you get the single-threaded version to work, it's time to
         // implement the concurrent version. Good luck :)
+
 
     }
 
     /*
-    This function generates all the intervals for merge sort iteratively, given 
+    This function generates all the intervals for merge sort iteratively, given
     the range of indices to sort. Algorithm runs in O(n).
 
     Parameters:
@@ -62,20 +154,20 @@ public class Main {
     }
 
     /*
-    This function performs the merge operation of merge sort.
+   This function performs the merge operation of merge sort.
 
-    Parameters:
-    array : vector<int> - array to sort
-    s     : int         - start index of merge
-    e     : int         - end index (inclusive) of merge
-    */
+   Parameters:
+   array : vector<int> - array to sort
+   s     : int         - start index of merge
+   e     : int         - end index (inclusive) of merge
+   */
     public static void merge(int[] array, int s, int e) {
         int m = s + (e - s) / 2;
         int[] left = new int[m - s + 1];
         int[] right = new int[e - m];
         int l_ptr = 0, r_ptr = 0;
-        for(int i = s; i <= e; i++) {
-            if(i <= m) {
+        for (int i = s; i <= e; i++) {
+            if (i <= m) {
                 left[l_ptr++] = array[i];
             } else {
                 right[r_ptr++] = array[i];
@@ -83,13 +175,53 @@ public class Main {
         }
         l_ptr = r_ptr = 0;
 
-        for(int i = s; i <= e; i++) {
-            if(r_ptr == e - m || left[l_ptr] <= right[r_ptr]) {
+        for (int i = s; i <= e; i++) {
+            // no more elements on left half
+            if (l_ptr == m - s + 1) {
+                array[i] = right[r_ptr];
+                r_ptr++;
+
+                // no more elements on right half or left element comes first
+            } else if (r_ptr == e - m || left[l_ptr] <= right[r_ptr]) {
                 array[i] = left[l_ptr];
                 l_ptr++;
             } else {
                 array[i] = right[r_ptr];
                 r_ptr++;
+            }
+        }
+        /*
+        System.out.println("Intervals: [" + s + ", " + e + "]");
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+        System.out.print("\n");
+
+         */
+    }
+
+    static class thread implements Runnable {
+        private int[] array;
+        private BlockingQueue<Interval> queue;
+
+        public thread(int[] array, BlockingQueue<Interval> queue) {
+            this.array = array;
+            this.queue = queue;
+        }
+
+        @Override
+        public void run() {
+            try {
+                Interval interval;
+                do {
+                    interval = queue.poll(1, TimeUnit.SECONDS);
+
+                    if (interval != null)
+                        merge(array, interval.getStart(), interval.getEnd());
+                }
+                while (interval != null);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
             }
         }
     }
@@ -120,3 +252,4 @@ class Interval {
         this.end = end;
     }
 }
+
